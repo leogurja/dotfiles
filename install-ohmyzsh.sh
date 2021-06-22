ab732727f6960176fb8affcc61e3edec5206e7bc
Leonardo Gurgel <lgurgel@id.uff.br>
2021-06-22 18:06:40 -0300

    22/06/21

--- install-ohmyzsh.sh
+++ install-ohmyzsh.sh
@@ -1,4 +1,1 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"

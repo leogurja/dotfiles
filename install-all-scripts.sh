ab49bb6d9dfac2d1c11a20d17479361de7371a72
Leonardo Gurgel <lgurgel@id.uff.br>
2021-12-21 18:46:03 +0000

    add install-all-scripts

---
+++ install-all-scripts.sh
@@ -0,0 +1,6 @@
+./install-nvm.sh && \
+./install-rustup.sh && \
+./install-sdkman.sh && \
+./install-oh-my-zsh.sh && \
+./install-zsh-plugins.sh && \
+./make-symlinks.sh
--- install-nvm.sh
+++ install-nvm.sh
@@ -1,1 +1,1 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash

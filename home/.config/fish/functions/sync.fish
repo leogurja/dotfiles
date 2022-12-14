e746e35451f8c1dc22ab5820fa6b7f838400717b
Leonardo Gurgel <lgurgel@id.uff.br>
2022-12-14 15:04:22 -0300

    colorsync output

--- home/.config/fish/functions/sync.fish
+++ home/.config/fish/functions/sync.fish
@@ -1,6 +1,12 @@
 function sync
+  set -l CYAN '\033[0;36m'
+  set -l NC '\033[0m'
+  echo -e "$CYAN### rpm-ostree ###$NC"
   rpm-ostree update 
+  echo -e "\n$CYAN### flatpak ###$NC"
   flatpak update -y 
+  echo -e "\n$CYAN### asdf ###$NC"
   asdf update && asdf plugin update --all
+  echo -e "\n$CYAN### astronvim ###$NC"
   nvim +TSUpdate +AstroUpdate +q 
 end

6efbb03db8935f7c8522e712b8ff3b8bd6f4a332
Leonardo Gurgel <lgurgel@id.uff.br>
2023-03-02 10:57:28 -0300

    a

--- astro-deps.sh
+++ astro-deps.sh
@@ -1,2 +1,3 @@
 go install github.com/dundee/gdu/v5/cmd/gdu@latest && go install github.com/jesseduffield/lazygit@latest
-cargo install ripgrep bottom tree-sitter-cli
+sudo dnf copr enable atim/bottom -y
+sudo dnf install bottom tree-sitter-cli ripgrep

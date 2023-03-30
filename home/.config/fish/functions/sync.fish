a53474016d21a51c2391d39d56f17e00fcb144c9
Leonardo Gurgel <lgurgel@id.uff.br>
2023-03-30 09:11:24 -0300

    b

--- home/.config/fish/functions/sync.fish
+++ home/.config/fish/functions/sync.fish
@@ -1,8 +1,8 @@
 function sync
   set -l CYAN '\033[0;36m'
   set -l NC '\033[0m'
-  echo -e "$CYAN### pacman ###$NC"
-  paru -Syu
+  echo -e "$CYAN### dnf ###$NC"
+  sudo dnf update -y
   echo -e "\n$CYAN### flatpak ###$NC"
   flatpak update -y 
   echo -e "\n$CYAN### asdf ###$NC"

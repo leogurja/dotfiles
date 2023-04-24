70ab2d79ac3369acc33389256e53c98f341a8fd0
Leonardo Gurgel <lgurgel@id.uff.br>
2023-04-24 17:28:22 -0300

    update setup-astro

--- setup-astro.sh
+++ setup-astro.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo dnf copr enable atim/lazygit -y
-sudo dnf install -y lazygit bottom ripgrep neovim
+sudo dnf install -y lazygit bottom ripgrep neovim cargo go
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim

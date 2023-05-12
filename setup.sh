1798eef92d0bf858accc8a39cc3225a44368dd74
Leonardo Gurgel <lgurgel@id.uff.br>
2023-05-12 17:44:19 -0300

    update setup

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo nala purge seahorse switcheroo-control gnome-remote-desktop gnome-logs gnom
   gnome-characters gnome-calculator nautilus-extension-gnome-terminal evince eog nano \
   vim-tiny update-manager update-notifier gnome-power-manager \
   software-properties-gtk yelp gnome-startup-applications gnome-shell-extension-desktop-icons-ng \
-  gnome-shell-extension-ubuntu-dock gnome-terminal network-manager-gnome
+  gnome-shell-extension-ubuntu-dock gnome-terminal network-manager-gnome snapd
 sudo nala autoremove
 
 # other
@@ -20,17 +20,12 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 gh auth login
 gh repo clone gurgelio/dotfiles
 
-# snap
-sudo snap install code --classic
-sudo snap install spotify discord slack insomnia
-sudo snap remove snap-store
-
 # flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install com.github.tchx84.Flatseal com.github.wwmm.easyeffects com.usebottles.bottles \
   com.valvesoftware.Steam de.haeckerfelix.Fragments org.freedesktop.Piper org.gnome.Boxes \
   org.gnome.Calculator org.gnome.Evince org.gnome.FileRoller org.gnome.Loupe org.gnome.NautilusPreviewer \
-  org.gnome.Totem org.kde.krita
+  org.gnome.Totem org.kde.krita com.spotify.Client com.discord.Discord rest.insomnia.Insomnia
 
 # trocar de shell padrão
 sudo chsh -s /usr/bin/fish

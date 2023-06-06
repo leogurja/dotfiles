f3d6724f4d08cc32ab277c5fd1bada344d3e77e4
Leonardo Gurgel <lgurgel@id.uff.br>
2023-06-05 22:45:31 -0300

    vanilla os

--- setup.sh
+++ setup.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# dnf repos
-sudo rm /etc/yum.repos.d/{fedora-cisco-openh264.repo,_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo,google-chrome.repo,rpmfusion-nonfree-nvidia-driver.repo,rpmfusion-nonfree-steam.repo,fedora-updates-testing-modular.repo,fedora-updates-testing.repo}
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-
 # packages
-sudo dnf install neofetch gh libratbag-ratbagd neovim fish podman code
+sudo abroot exec apt install ratbagd gamemode epiphany-browser- totem- gnome-music-
 curl -sS https://starship.rs/install.sh | sh
+curl https://az764295.vo.msecnd.net/stable/b3e4e68a0bc097f0ae7907b217c1119af9e03435/code_1.78.2-1683731010_amd64.deb -o ~/Downloads/code.deb
 
-# set defaults
-sudo usermod -s $(which fish) $USER
+# code
+apx install ~/Downloads/code.deb
+apx export code
+apx export --bin code
 xdg-mime default code.desktop text/plain
 
-
 # flatpak
-flatpak remote-delete fedora
-flatpak remote-delete fedora-testing
-flatpak remote-delete flathub
-flatpak remote-add --user flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install com.discordapp.Discord com.github.marhkb.Pods com.github.tchx84.Flatseal \
+  com.github.wwmm.easyeffects com.spotify.Client com.usebottles.bottles com.valvesoftware.Steam \
+  org.freedesktop.Piper org.gnome.Boxes org.gnome.Calendar org.gnome.DejaDup org.gnome.Evince \
+  org.gnome.Totem org.gnome.baobab org.mozilla.firefox
 flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
-systemctl --user enable --now podman.socket # enable podman socket for com.github.marhkb.Pods
 
 git submodule init && git submodule update
 

a0c27c7a4704d8c4ad289e4d5eb12d38b5e99944
Leonardo Gurgel <leonardogurgelmf@gmail.com>
2024-08-13 11:24:09 -0300

    add flatpak configs

--- setup/02-setup-symlinks.sh
+++ setup/02-setup-symlinks.sh
@@ -14,22 +14,38 @@ files="
   .local/share/asdf
   .local/share/flatpak/overrides
   .local/share/fonts
+  .var/app/com.google.Chrome/config/chrome-flags.conf
+  .var/app/com.github.wwmm.easyeffects/config/easyeffects/input/solocast.json
+"
+
+hard_link_files="
+  .var/app/com.raggesilver.BlackBox/config/glib-2.0/settings/keyfile
 "
 
 mkdir -p "$backup"
 
 for file in $files; do
-  if [ -e "$HOME/$file" ] && [ ! -L "$HOME/$file" ]; then
-    echo "Salvando $HOME/$file antigo"
-    mv $HOME/$file "$backup"
-  fi
-
   if [ -L "$HOME/$file" ]; then
     rm "$HOME/$file"
   fi
 
+  if [ -e "$HOME/$file" ]; then
+    echo "Salvando $HOME/$file antigo"
+    mv $HOME/$file "$backup"
+  fi
+
   echo "$home/$file -> $HOME/$file"
   ln -sf "$home/$file" $HOME/$file
+done
+
+for file in $hard_link_files; do
+  if [ -e "$HOME/$file" ]; then
+    echo "Salvando $HOME/$file antigo"
+    mv $HOME/$file "$backup"
+  fi
+
+  echo "AS HARD LINK: $home/$file -> $HOME/$file"
+  cp -l "$home/$file" $HOME/$file
 done
 
 git submodule init && git submodule update

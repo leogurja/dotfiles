79e8de58e75218cb2341ae01a0470f71ff67aab7
Leonardo Gurgel <leo@gurgel.io>
2025-07-26 18:11:58 -0300

    switch to ubuntu

--- home/.config/fish/functions/update.fish
+++ home/.config/fish/functions/update.fish
@@ -1,8 +1,9 @@
 function update
   set_color -o cyan
-  echo "### DNF ###"
+  echo "### APT ###"
   set_color normal
-  sudo dnf update -y
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
 
   set_color -o cyan
   echo -e "\n### FLATPAK ###"
---
+++ setup/01-ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ROOT=$(git rev-parse --show-toplevel)
+
+# apt packages
+sudo apt-get install -y $(cat $ROOT/packages/apt-install.txt)
+sudo apt-get purge -y $(cat $ROOT/packages/apt-purge.txt)
+sudo apt-get autoremove -y
+
+# flatpak
+flatpak remote-add --user flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y $(cat $ROOT/packages/flatpak-install.txt)
+
+# add user to docker group
+sudo usermod -aG docker $USER
+
+# fix dual boot clock
+timedatectl set-local-rtc 1
+
+# mise
+curl https://mise.run | sh
+
+chsh -s $(which fish)

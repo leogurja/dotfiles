90e2dc5bbc13b1917fb7293a6da687ea3f5d3733
Leonardo Gurgel <leo@gurgel.io>
2025-12-30 15:21:53 -0300

    a

---
+++ home/.config__fish/functions/cursor.fish
@@ -0,0 +1,3 @@
+function cursor --wraps=/home/gurja/AppImages/cursor.appimage --description 'alias cursor=/home/gurja/AppImages/cursor.appimage'
+    /home/gurja/AppImages/cursor.appimage $argv
+end

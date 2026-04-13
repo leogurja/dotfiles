7ce882342126d1c149365aec7ada4adcb7651a63
Leonardo Gurgel <leo@gurgel.io>
2026-04-13 12:45:30 -0300

    melhorar completions

--- home/.config__fish/conf.d/brew.fish
+++ home/.config__fish/conf.d/brew.fish
@@ -1,1 +1,7 @@
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+
+set -l _brew_fish_vendor "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
+if test -d "$_brew_fish_vendor"
+    and not contains -- "$_brew_fish_vendor" $fish_complete_path
+    set -gx fish_complete_path $fish_complete_path "$_brew_fish_vendor"
+end
